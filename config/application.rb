0dc87121018962d4321c7da1ae4bc72b64263939
Yuriy <yuriy@testnest.co>
2016-09-19 18:55:16 +0300

    add slim as template engine

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Watech
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.template_engine :slim
+    end
   end
 end
