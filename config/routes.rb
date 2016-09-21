4641a473ea3b57fbd7fa629fa86949cde4258316
Yuriy <yuriy@testnest.co>
2016-09-21 16:35:25 +0300

    add context_items routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   root to: 'admin/rules#index'
 
   namespace :admin do
-    resources :rules, only: [:index, :update]
+    resources :rules, only: [:index, :update] do
+      resources :context_items, only: :create
+    end
   end
 
 end
