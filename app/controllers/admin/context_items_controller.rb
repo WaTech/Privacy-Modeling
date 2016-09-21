938fa7a09cbcd49402829dd49cd74a0e1dec7e39
Yuriy <yuriy@testnest.co>
2016-09-21 16:34:01 +0300

    add context items controller with create action only

---
+++ app/controllers/admin/context_items_controller.rb
@@ -0,0 +1,17 @@
+class Admin::ContextItemsController < ApplicationController
+  def create
+    context_item = ContextItem.new context_item_params.merge(rule_id: params[:rule_id])
+
+    if context_item.save
+      render json: {}, status: :ok
+    else
+      render json: {}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def context_item_params
+    params.require(:context_item).permit(:category, :description)
+  end
+end
