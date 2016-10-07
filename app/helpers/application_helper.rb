917ba4212975637f6fb7505d55bd2e59e150f1aa
Yuriy <yuriy@testnest.co>
2016-10-07 16:16:47 +0300

    enable navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def section_nav_tag nav_step
+    content_tag_class = nav_step == @step || past_step?(nav_step) ? 'active' : nil rescue 'active'
+    content_tag :section, I18n.t(nav_step), class: content_tag_class
+  end
 end
