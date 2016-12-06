471d206a697c5ffcdce8a544bfa9ef4b07ac24f9
Andrew Kozey <kozeyandrey@gmail.com>
2016-12-06 21:36:04 +0200

    Added parameter "law_type" to require params + removed filter_of_law method

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,14 +6,19 @@ class DashboardsController < ApplicationController
       use_item_id: dashboard_params[:use_item_ids]
     }
 
-    @grouped_rules = Rule.where(condition).group_by(&:restriction)
-  end
+    @grouped_rules = if params[:law_type].present?
+                  Rule.where(condition).group_by(&:restriction)
+                else
+                  Rule.where(condition).group_by(&:restriction).filter_of_law(dashboard_params[:law_type])
+                end
 
-  def filter_of_law
-    @grouped_rules.filter_of_law(params[:type])
+    respond_to do |format|
+      format.html  # show.html.slim
+      format.json  { render :json => @grouped_rules }
+    end
   end
 
   def dashboard_params
-    @dashboard_params ||= params.require(:dashboard).permit(category_ids: [], pii_ids: [], use_item_ids: [])
+    @dashboard_params ||= params.require(:dashboard).permit(:law_type, category_ids: [], pii_ids: [], use_item_ids: [])
   end
 end
